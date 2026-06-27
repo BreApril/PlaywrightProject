eb9310bd9fb2caf927f61f7c1100839315b8586c
haybre <haybreapril@gmail.com>
2026-06-27 19:44:31 +0200

    created TestData page

---
+++ src/data/TestData.ts
@@ -0,0 +1,14 @@
+export const invoiceData = {
+    clientName: 'April Pty Ltd',
+    clientAddress: '123 Fake Street, Cape Town, 8001',
+    notes: 'This is a test invoice description',
+    dueDate: '2026-06-30',
+    status: 'Paid',
+    expectedTotal: 'R2800',
+    courses: [
+        'Testing Course',
+        'API Testing with Postman – Fundamentals',
+        'Testing Course',
+        'API Testing with Postman – Fundamentals'
+    ]
+}
